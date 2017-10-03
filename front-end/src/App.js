f6087e2cf337db9c83036a85c007a31911bf2d15
Alex Johnson <david.johnsoniv@gmail.com>
2017-10-03 05:21:42 -0600

    adding the router

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,18 +1,48 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import axios from 'axios'
+import Body from './components/body.js'
+
+const url = 'http://localhost:1337/api/shelf/A'
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      myArray: []
+    };
+  }
+
+  // get() {
+  // axios
+  //   .get('http://localhost:1337/api/shelf/A')
+  //   .then((response) => {console.log(response)})
+  // }
+
+  get() {
+    axios
+      .get(url).then((response) => {this.setState({myArray: response.data});
+        console.log("Here is the response: ", response)})
+      .catch((response) => {
+        console.log("We have an issue.")})
+  }
+
+  componentWillMount() {
+    this.get();
+  }
+
   render() {
+
+    var arrayItem = this.state.myArray.map((item) => {
+      return (
+        <p key={item.entryid}>{item.entryid}{item.itemname}</p>
+      )
+    })
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Body />
       </div>
     );
   }
---
+++ front-end/src/components/body.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Body extends Component {
+
+  render() {
+    return(
+      <h1>This is the body</h1>
+    )
+  }
+}
+
+export default Body;
